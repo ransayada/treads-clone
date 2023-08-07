a450bd871c176db46fa51d60a4041e180207e56d
ransayada <ran.sayada@gmail.com>
2023-08-07 12:09:39 +0300

    wip finish left side bar

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import { sidebarLinks } from "../../constants/index";
+import Image from "next/image";
 const LeftSideBar = () => {
-  return <h1>LeftSideBar</h1>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => (
+          <Link href={link.route} key={link.label} className="leftsidebar_link">
+            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+            <p className="text-light-1 max-lg:hidden">{link.label}</p>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default LeftSideBar;
