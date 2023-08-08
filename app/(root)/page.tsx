9db02822dba497fca9f511616df054766001510a
ransayada <ran.sayada@gmail.com>
2023-08-08 15:31:07 +0300

    wip home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
-//app/page.tsx
+import { fetchPosts } from "@/lib/actions/thread.action";
 import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+
+  console.log(result);
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -32,3 +32,36 @@ export async function createThread({text,author,communityId,path}:Params){
 
 }
 }
+
+export async function fetchPosts (pageNumber= 1, pageSize = 20){
+    try{
+    connectToDB();
+    //coculate the number of posts to skip 
+        const  skipAmount = (pageNumber-1)* pageSize;
+
+    //fetch all threads without parents (top level thread)
+    const postsQuery = Thread.find({
+        parentId: {$in: [null, undefined]}
+    }).sort({createdAt: 'desc'}).skip(skipAmount).limit(pageSize).populate({
+        path:'author', model: User
+    }).populate({
+        path: 'children' , populate: {
+            path: 'author',
+            model: User,
+            select: "_id name parentId image"
+        }
+    })
+
+    const totalPostsCount = await Thread.countDocuments({
+        parentId: {$in: [null, undefined]}
+    })
+
+    const posts = await postsQuery.exec()
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return {posts, isNext}
+    }catch(error:any){
+        throw new Error(`Error while fetching thread posts: ${error}`)
+    }
+}
